120ba6a2b2cadd26586c1cf0d49550648f73a0fe
farukarslan <o.farukarslan0@gmail.com>
2021-02-21 15:50:04 +0300

    Proje dosyası ekle.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
