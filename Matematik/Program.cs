a019ad46eb58844fe52425a9a2e3e4551e49a405
farukarslan <o.farukarslan0@gmail.com>
2021-02-21 16:14:05 +0300

    Parametre ile metot kullanımı

---
+++ Matematik/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortİslem = new DortIslem();
+            dortİslem.Topla(5, 6);
+        }
+    }
+}
