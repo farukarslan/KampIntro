6ea462bcedfd208c9957e4422f0dee20cfa5d2c9
farukarslan <o.farukarslan0@gmail.com>
2021-02-21 16:17:42 +0300

    Değer ve Referans Tipler

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //sayi1 ?? 30
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1[0] ?? 999
+
+
+            /* int, decimal,floot,double,bool... = değer tip    (değer tiplerde bellekte sadece stack kullanılarak değişkenin değeri tutulur)
+               array,class,interface             = referans tip (referans tiplerde ise bellekte stack ve heap kullanılarak adres tutulur ve eşitlenir)*/
+        }
+    }
+}
