e8a50cb739cef88da4ec9c07da18824960e71b71
farukarslan <o.farukarslan0@gmail.com>
2021-02-21 16:05:49 +0300

    Metot Kullanımı

---
+++ Metotlar/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { "Elma", "Karpuz" };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler) //urunler dizisini tek tek geziyor //en soldaki veri tipi
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-------------------");
+            }
+
+            Console.WriteLine("---------------Metotlar-----------------");
+
+            //instance - örnek
+            //encapsulation - Kapsulleme - ayrı ayrı yazmak yerine class kullanarak encapsulation yapıyoruz.
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12, 8);   // Metodumuzda bir parametre değişikliği yaptığımızda burayı da değiştirmemiz gerekiyor.
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12, 9);      // Eğer ki değiştirmezsek burada patlicak ve hata vericek.
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12, 9); // Bu kullanım şekli yerine class kullandığımızda yeni eklenen property
+                                                                       // için metodu düzeltmemiz gerekmiyor. Çünkü class bütün propertyleri kendi içerisinde barındırıyor.
+
+        }
+    }
+}
+
+//Dont repeat yourself(DIY) - tekrar etme - clean code - best practice
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention - isimlendirme
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi: " + urun.Adi);
+        }
+
+        public void Ekle2(string urunadi, string urunaciklamasi, double fiyati, int stokadedi)
+        {
+            Console.WriteLine("Sepete eklendi: " + urunadi);
+        }
+    }
+}
