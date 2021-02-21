36eee503d7dd935181c1616de7c7b7e224e94137
farukarslan <o.farukarslan0@gmail.com>
2021-02-21 15:55:49 +0300

    C# Class'dan Ürün Listeleme

---
+++ KampOdev2/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KampOdev2
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string AyakkabiMarkası { get; set; }
+        public string AyakkabiNo { get; set; }
+        public string Renk { get; set; }
+        public string Cinsiyet { get; set; }
+        public int Fiyat { get; set; }
+        public string AyakkabiTipi { get; set; }
+    }
+}
---
+++ KampOdev2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace KampOdev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.AyakkabiMarkası = "Nike";
+            product1.AyakkabiNo = "35-42";
+            product1.Renk = "Siyah";
+            product1.Cinsiyet = "Erkek";
+            product1.Fiyat = 250;
+            product1.AyakkabiTipi = "Spor";
+
+            Product product2 = new Product();
+            product2.Id = 2;
+            product2.AyakkabiMarkası = "Adidas";
+            product2.AyakkabiNo = "35-39";
+            product2.Renk = "Pembe";
+            product2.Cinsiyet = "Kadın";
+            product2.Fiyat = 400;
+            product2.AyakkabiTipi = "Spor";
+
+            Product product3 = new Product();
+            product3.Id = 3;
+            product3.AyakkabiMarkası = "Greyder";
+            product3.AyakkabiNo = "35-44";
+            product3.Renk = "Kum yeşili";
+            product3.Cinsiyet = "Unisex";
+            product3.Fiyat = 750;
+            product3.AyakkabiTipi = "Bot";
+
+            Product product4 = new Product();
+            product4.Id = 4;
+            product4.AyakkabiMarkası = "Skechers";
+            product4.AyakkabiNo = "39";
+            product4.Renk = "Mavi";
+            product4.Cinsiyet = "Kadın";
+            product4.Fiyat = 650;
+            product4.AyakkabiTipi = "Spor";
+
+            Product[] products = new Product[] { product1, product2, product3, product4 };
+            foreach (var product in products)
+            {
+                Console.WriteLine("Ayakkabı Markası: " + product.AyakkabiMarkası + "\nAyakkabı Tipi: " + product.AyakkabiTipi + "\nAyakkabı Rengi: " + product.Renk
+                    + "\nAyakkabı Numarası: " + product.AyakkabiNo + "\nCinsiyet: " + product.Cinsiyet + "\nAyakkabı Fiyatı: " + product.Fiyat);
+                Console.WriteLine("---------------------------------------------------");
+
+            }
+        }
+    }
+}
