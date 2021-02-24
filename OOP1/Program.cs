4b43d7d69f4ba75e842ea03ca4dbeae4334fc960
farukarslan <o.farukarslan0@gmail.com>
2021-02-24 14:08:04 +0300

    Object Oriented Programming

---
+++ OOP1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2; //2. Kategori mobilya
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5,
+                UnitsInStock = 5, ProductName = "Kalem", UnitPrice = 35};
+
+            //PascalCase   //camelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+
+        }
+    }
+}
