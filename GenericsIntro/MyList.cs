e2fd3b45f0c8e558f53964dbe8072dbd53a48e76
farukarslan <o.farukarslan0@gmail.com>
2021-02-24 18:30:48 +0300

    Kendi Koleksiyonumuzu(List) Oluşturma

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T> //T  bizim verdiğimiz tipe karşılık gelir.Sadece bir tipte değil diğer tiplerde de eleman eklememizi sağlar.
+    {
+        T[] items;
+        //constructor bloğu(classı newlediğimiz anda çalışan kod bloğu)
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;              //Aşağıdaki satırda eleman sayısını arttırmak için new yaptığımızda arrayin referans değeri değişiyor.
+            items = new T[items.Length + 1];    //Önceki bilgilerin kaybolmaması için de geçici dizide önceki referansı tutuyoruz.
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i]; // tempArray'de tutuan items'ın eski değerlerini items dizisine geri veriyoruz.
+            }
+
+            items[items.Length - 1] = item; // aslında eklenmek istenen yeni elemanı şimdi burada ekledik
+        }
+        public int Length
+        {
+            get { return items.Length; }
+        }
+        public T[] Items
+        {
+            get { return items; }
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Eğer C#'da List diye bir şey olmasaydı kendi koleksiyonumuzu nasıl yazardık?
+
+
+            MyList<string> isimler = new MyList<string>(); //Arkaplanda T = string yaptık.
+            isimler.Add("Ömer");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Ali");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
