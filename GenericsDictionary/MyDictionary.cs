0978331047369cf3ee5ff60c1c9016ff9a0c1e03
farukarslan <o.farukarslan0@gmail.com>
2021-02-27 19:29:44 +0300

    Kendi Dictionary Sınıfımızı Oluşturma

--- GenericsDictionary/MyDictionary.cs
+++ GenericsDictionary/MyDictionary.cs
@@ -4,8 +4,41 @@ using System.Text;
 
 namespace GenericsDictionary
 {
-    class MyDictionary<T_Key,T_Value>
+    class MyDictionary<TKey, TValue>
     {
-        
+        TKey[] _kArray;
+        TValue[] _vArray;
+        public MyDictionary()
+        {
+            _kArray = new TKey[0];
+            _vArray = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] _kTempArray = _kArray;
+            TValue[] _vTempArray = _vArray;
+            _kArray = new TKey[_kArray.Length + 1];
+            _vArray = new TValue[_vArray.Length + 1];
+
+            for (int i = 0; i < _kTempArray.Length; i++)
+            {
+                _kArray[i] = _kTempArray[i];
+                _vArray[i] = _vTempArray[i];
+            }
+            _kArray[_kArray.Length - 1] = key;
+            _vArray[_vArray.Length - 1] = value;
+        }
+        public void List()
+        {
+            for (int j = 0; j < _kArray.Length; j++)
+            {
+                Console.WriteLine("Ürün adı : " + _kArray[j] + ", " + "Stok adedi : " + _vArray[j]);
+            }
+        }
+        public int Length
+        {
+            get { return _vArray.Length; }
+        }
     }
 }
--- GenericsDictionary/Program.cs
+++ GenericsDictionary/Program.cs
@@ -6,6 +6,15 @@ namespace GenericsDictionary
     {
         static void Main(string[] args)
         {
+            MyDictionary<string, int> urunStok = new MyDictionary<string, int>();
+            Console.WriteLine("---------Ürün Listesi---------");
+            urunStok.Add("CAT Bot", 14);
+            urunStok.Add("Nike Koşu ayakkabısı", 30);
+            urunStok.Add("Adidas Sneaker", 23);
+            urunStok.Add("Nike Terlik", 15);
+
+            urunStok.List();
+            Console.WriteLine("\nDizi içindeki eleman sayısı : "+urunStok.Length);
             
         }
     }
